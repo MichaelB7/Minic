0089af0eb86970c00e9c79bd7192c0a93b9963e3
MichaelB7 <michaelbyrne1978@gmail.com>
2019-07-04 20:05:00 -0400

    macOS build script added

---
+++ macOS.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#dir=$(readlink -f $(dirname $0))
+dir=~/github/minic
+d="-DDEBUG_TOOL"
+v="dev"
+t="-march=native"
+#t="-mpopcnt -msse4.2 -mavx -mavx2 -mbmi -mbmi2"
+
+if [ -n "$1" ] ; then
+   v=$1
+   shift
+fi
+
+if [ -n "$1" ] ; then
+   t=$1
+   shift
+fi
+
+g++ -v
+echo "version $v"
+echo "definition $d"
+echo "target $t"
+exe=minic_${v}_macOS_x64
+if [ "$t" != "-march=native" ]; then
+   tname=$(echo $t | sed 's/-m//g' | sed 's/arch=//g' | sed 's/ /_/g')
+   exe=${exe}_${tname}
+fi
+echo "Building $exe"
+g++  -fprofile-generate -Wall -Wno-char-subscripts -Wno-reorder $d -DNDEBUG -O3 -flto $t --std=c++11 minic.cc -o $dir/Dist/$exe -lpthread -IFathom/src
+$dir/Dist/$exe -analyze "r2q1rk1/p4ppp/1pb1pn2/8/5P2/1PBB3P/P1PPQ1P1/2KR3R b - - 1 14" 20
+g++  -fprofile-use $d -DNDEBUG -O3 -flto $t --std=c++11 minic.cc -o $dir/Dist/$exe -lpthread -IFathom/src
+rm *.gcda
+cp $dir/Dist/$exe ~/cluster.mfb/minic
+
